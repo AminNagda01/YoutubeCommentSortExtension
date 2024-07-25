deaa7afdd394fe9355e9884ca8d4dc31b0165ff9
Muhammad Amin <aminworkm@gmail.com>
2024-07-25 04:28:36 -0400

    Added date checking client side, default date

--- popup/popup.js
+++ popup/popup.js
@@ -2,19 +2,24 @@
 const startDateElement = document.getElementById("startDate")
 const endDateElement = document.getElementById("endDate")
 const goButtonElement = document.getElementById("goButton")
+const todaysDate = new Date()
 let startIsValid = false 
 let endIsValid = false 
 
-function getDateFromHtml() {
-    console.log(document.body);
-}
-  
-if (document.readyState !== "loading") {
-    getDateFromHtml();
-} else {
-    // Loading hasn't finished yet
-    document.addEventListener("DOMContentLoaded", getDateFromHtml);
-}
+window.addEventListener("load", (event) => {
+    var month = todaysDate.getMonth() + 1; 
+    var day = todaysDate.getDate(); 
+    var year = todaysDate.getFullYear(); 
+    if (month < 10) {
+        month = "0" + month; 
+    }
+    if (day < 10) {
+        day = "0" + day;
+    }
+
+    const currDateOnPopup = document.querySelector('#endDate'); 
+    currDateOnPopup.value = year + "-" + month + "-" + day; 
+});
 
 // Modeled from Youtube API docs 
 function getCurrentTab(callback) {
@@ -41,15 +46,37 @@ goButtonElement.onclick = function() {
         var videoID = youtube_parser(currentUrl)   
         if (videoID === false) {
             console.log("Invalid link (move to the youtube page then open this extension)")
-            return //TODO: Test this 
+            return
         } 
 
         // If the dates are actual values (not left blank)
         if (startDateElement.value && endDateElement.value) {
             // Check if the start and end date is some time between or equal to the published date and today, and that end date is after or the same day as start
-            console.log("Start Date: ", startDateElement.value);
+            console.log("Start Date: ", startDateElement.value); // Start Date:  2024-07-01
             console.log("End Date: ", endDateElement.value);
-            console.log("Fetching data and outputting it");
+            
+            var startDateArr = startDateElement.value.split('-') 
+            var endDateArr = endDateElement.value.split('-')
+            if (parseInt(startDateArr[0]) < 2005 || 
+                parseInt(startDateArr[0]) > todaysDate.getFullYear() || 
+                parseInt(endDateArr[0]) < 2005 || 
+                parseInt(endDateArr[0]) > todaysDate.getFullYear() ||
+                parseInt(startDateArr[1]) < 4 || 
+                parseInt(startDateArr[1]) > (todaysDate.getMonth() + 1) || 
+                parseInt(endDateArr[1]) < 4 || 
+                parseInt(endDateArr[1]) > (todaysDate.getMonth() + 1) ||
+                parseInt(startDateArr[2]) < 23 || 
+                parseInt(startDateArr[2]) > todaysDate.getDate() || 
+                parseInt(endDateArr[2]) < 23 || 
+                parseInt(endDateArr[2]) > todaysDate.getDate()) { 
+                    console.log("Invalid year") // TODO: Return error to user through html 
+            }
+            else {
+                // Call API  
+                return
+            }
+
+
         } else {
             console.log("One or more dates are not valid");
         }
