152a1e6b527681ee31eda30f63997158b7c45973
Muhammad Amin <aminworkm@gmail.com>
2024-08-09 03:29:14 -0400

    #1 proper json response for only 100 comments, needs while loop and txt output

--- websever/app.py
+++ websever/app.py
@@ -6,7 +6,8 @@ import googleapiclient.discovery
 # TODO: ADD LOTS OF ERROR CHECKING 
 
 # Function that takes the video ID And returns all info 
-def youtubeCaller(**data):
+def youtubeCaller(nextPageToken, **data):
+    nextPageToken = nextPageToken
     videoID = data["videoIdentification"]
     start = data["startingDate"]
     end = data["endingDate"]
@@ -19,15 +20,24 @@ def youtubeCaller(**data):
     
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, developerKey = DEVELOPER_KEY)
+    
 
     request = youtube.commentThreads().list(
         part="snippet,replies",
         maxResults=100,
-        videoId=videoID
+        videoId=videoID,  
+        pageToken=nextPageToken
     )
     response = request.execute()
-
     return response
+
+
+def create_response_dict(commentsList): 
+
+    
+
+    return 
+
 
 # Application Factory. As far as ive seen, the biggest reason to do this is to 
 # test the app better. 
@@ -38,22 +48,39 @@ def create_app():
     # NOTE: Remove before deployment 
     app.config["DEBUG"] = True
 
-    print("weourhere")
-
     # Can also do @app.route('/stores', methods=['GET'])
     @app.post("/search")
     def get_store():
         recieved_data = request.get_json()
+        commentsList = youtubeCaller(nextPageToken="", **recieved_data) # First send blank
 
-        print(recieved_data)
+        comments = []
 
-        commentsList = youtubeCaller(**recieved_data)
+        for indexOfComment in commentsList["items"]:
+            dictionaryOfCommnets = {
+                "Comment": indexOfComment["snippet"]["topLevelComment"]["snippet"]["textDisplay"], 
+                "Posted": indexOfComment["snippet"]["topLevelComment"]["snippet"]['publishedAt'],
+                "Author": indexOfComment["snippet"]["topLevelComment"]["snippet"]["authorDisplayName"],
+            }
 
-        print("here")
+            if indexOfComment["snippet"]["totalReplyCount"] > 0: 
+                for indexOfReply in indexOfComment["replies"]["comments"]: 
+                   dictionaryOfCommnets["Replies"] = {
+                        "Comment": indexOfReply["snippet"]["textDisplay"],
+                        "Posted": indexOfReply["snippet"]["publishedAt"],
+                        "Author": indexOfReply["snippet"]["authorDisplayName"]
+                    }
+                   
+            comments.append(dictionaryOfCommnets)
+
+        # commentsToReturn = create_response_dict(commentsList)
+ 
+        # while True:
+        #     if commentsList["nextPageToken"]: #if it exists
+        #         commentsList = youtubeCaller(nextPageToken=commentsList["nextPageToken"], **recieved_data) # send the next page token 
+        #     else:
+        #         break
         
-        return {"message": commentsList}, 200
+        return {"message": comments}, 200
     
     return app
-
-# Send request to youtube API 
-# output data to txt file 
